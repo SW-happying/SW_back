c90b8370246d2902dd5348e5c07366e742e81a74
HXXBIN <gyqls0472@naver.com>
2024-10-18 13:59:57 +0900

    최종

--- controllers/ottusingController.js
+++ controllers/ottusingController.js
@@ -5,9 +5,9 @@ const getUsingRooms = async (req, res) => {
     const { userId } = req.params; 
     try {
         const joinedRooms = await EnterRoom.find({ userId }).populate('roomId');
-        const createdRooms = await ottRoom.find({ leaderId: userId }); 
-        const allRooms = [...joinedRooms, ...createdRooms];
-        res.status(200).json(allRooms);
+        // const createdRooms = await ottRoom.find({ leaderId: userId }); 
+        // const allRooms = [...joinedRooms, ...createdRooms];
+        res.status(200).json(joinedRooms);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: '방 목록을 가져오는 중 오류가 발생했습니다.' });
